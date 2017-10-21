f4701bb03bdb07db7068124f991d5d8988d94f78
Justin Pinner <justinpinner@gmail.com>
2017-10-21 11:03:24 +0100

    var -> const

--- js/game.js
+++ js/game.js
@@ -1,18 +1,18 @@
 // js/game.js
 
-var debug = true;
+const debug = true;
 
-var minNPC = 5;
-var fps = 30;
+const minNPC = 5;
+const fps = 30;
 
-var imageService = new ImageService();
+const imageService = new ImageService();
 
 class Game {
   constructor(playerName, shipName) {   
     this._gameObjects = [];
     this._background = new Background();
     // size and style background wrapper div
-    var bgWrapper = document.querySelector('#bgdiv');
+    const bgWrapper = document.querySelector('#bgdiv');
     if (bgWrapper) {
         bgWrapper.style.left = this._background.coordinates.x.toString() + 'px';
         bgWrapper.style.top = this._background.coordinates.y.toString() + 'px';
@@ -21,7 +21,7 @@ class Game {
         bgWrapper.style.backgroundColor = '#000000';
     }
     // size background container element to match canvas dimensions
-    var bgCanvasElement = document.querySelector(this._background.selector);
+    const bgCanvasElement = document.querySelector(this._background.selector);
     if (bgCanvasElement) {
         bgCanvasElement.style.left = this._background.coordinates.x.toString() + 'px';
         bgCanvasElement.style.top = this._background.coordinates.y.toString() + 'px';
@@ -30,7 +30,7 @@ class Game {
     }
 
     this._midground = new Midground();
-    var mgWrapper = document.querySelector('#mgdiv');
+    const mgWrapper = document.querySelector('#mgdiv');
     if (mgWrapper) {
         mgWrapper.style.left = this._midground.coordinates.x.toString() + 'px';
         mgWrapper.style.top = this._midground.coordinates.y.toString() + 'px';
@@ -39,7 +39,7 @@ class Game {
         mgWrapper.style.background = 'transparent';
     }
     // size mid-ground container element to match canvas dimensions
-    var mgCanvasElement = document.querySelector(this._midground.selector);
+    const mgCanvasElement = document.querySelector(this._midground.selector);
     if (mgCanvasElement) {
         mgCanvasElement.style.left = this._midground.coordinates.x.toString() + 'px';
         mgCanvasElement.style.top = this._midground.coordinates.y.toString() + 'px';
@@ -48,7 +48,7 @@ class Game {
     }
 
     this._viewport = new Viewport();
-    var vpWrapper = document.querySelector('#fgdiv');
+    const vpWrapper = document.querySelector('#fgdiv');
     if (vpWrapper) {
         vpWrapper.style.left = this._viewport.coordinates.x.toString() + 'px';
         vpWrapper.style.top = this._viewport.coordinates.y.toString() + 'px';
@@ -57,7 +57,7 @@ class Game {
         vpWrapper.style.background = 'transparent';
     }
     // size mid-ground container element to match canvas dimensions
-    var vpCanvasElement = document.querySelector(this._viewport.selector);
+    const vpCanvasElement = document.querySelector(this._viewport.selector);
     if (vpCanvasElement) {
         vpCanvasElement.style.left = this._viewport.coordinates.x.toString() + 'px';
         vpCanvasElement.style.top = this._viewport.coordinates.y.toString() + 'px';
@@ -116,7 +116,7 @@ Game.prototype.tick = function() {
     return;
   }
   this._viewport.clear();  
-  var deadAndAlive = this._gameObjects.partition(function(obj) {
+  const deadAndAlive = this._gameObjects.partition(function(obj) {
     return obj.disposable;
   });
   this._gameObjects = deadAndAlive[1];
@@ -127,39 +127,37 @@ Game.prototype.tick = function() {
     }
     this._gameObjects[i].updateAndDraw(debug);
     if (this._gameObjects[i] === this._playerShip) {
-      var uiCoord = document.querySelector(".ui.debug.coord");
+      const uiCoord = document.querySelector(".ui.debug.coord");
       if (uiCoord) {
         uiCoord.innerHTML = "<p>x:" + (this._playerShip.coordinates.x ? this._playerShip.coordinates.x.toFixed(1) : " ") + " y:" + (this._playerShip.coordinates.y ? this._playerShip.coordinates.y.toFixed(1) : " ") + "</p>";
       }
-      var uiVector = document.querySelector(".ui.debug.vector");
+      const uiVector = document.querySelector(".ui.debug.vector");
       if (uiVector) {
         uiVector.innerHTML = "<p>spd:" + (this._playerShip.speed ? this._playerShip.speed.toFixed(1) : " ") + " hdg:" + (this._playerShip.heading ? this._playerShip.heading.toFixed(1) : " ") + "</p>";
       }
-      var uiStatus = document.querySelector(".ui.debug.status");
+      const uiStatus = document.querySelector(".ui.debug.status");
       if (uiStatus) {
         uiStatus.innerHTML = "<p>status:" + this._playerShip.status + "</p>";
       }
-      var uiCondition = document.querySelector(".ui.debug.condition");
+      const uiCondition = document.querySelector(".ui.debug.condition");
       if (uiCondition) {
         uiCondition.innerHTML = "<p>shields:" + this._playerShip.shield.charge + "% armour:" + this._playerShip.armour + " hull:" + this._playerShip.hullIntegrity + "%</p>";
       }
-      var uiInputs = document.querySelector(".ui.debug.inputs");
+      const uiInputs = document.querySelector(".ui.debug.inputs");
       if (uiInputs) {
         uiInputs.innerHTML = "<p>thrust:" + (this._playerShip.thrust ? this._playerShip.thrust.toFixed(1) : " ") + "</p>";
       }      
     }
   }
-  var uiNpcs = document.querySelector(".ui.debug.npcs");
+  const uiNpcs = document.querySelector(".ui.debug.npcs");
   if (uiNpcs) {
     uiNpcs.innerHTML = "<p>NPCs:" + npcCount + "</p>";
   }
   // spawn new / maintain min. NPC ships
   for (var i = npcCount; i < minNPC; i++) {
-    var spawnShipType = ShipTypes[Object.keys(ShipTypes)[Math.floor(rand(Object.keys(ShipTypes).length))]];
-    var spawnShipRole = ShipRoles[Object.keys(ShipRoles)[Math.floor(rand(Object.keys(ShipRoles).length - 1))]];
-    var newShip = new Ship(spawnShipType, 'NPC' + i, spawnShipRole);
-    //newShip.coordinates.x = this._playerShip.coordinates.x + rand(this.maxSpawnDistanceX, incNegatives = true);
-    //newShip.coordinates.y = this._playerShip.coordinates.y + rand(this.maxSpawnDistanceY, incNegatives = true);
+    const spawnShipType = ShipTypes[Object.keys(ShipTypes)[Math.floor(rand(Object.keys(ShipTypes).length))]];
+    const spawnShipRole = ShipRoles[Object.keys(ShipRoles)[Math.floor(rand(Object.keys(ShipRoles).length - 1))]];
+    const newShip = new Ship(spawnShipType, 'NPC' + i, spawnShipRole);
     this._gameObjects.push(newShip);
   }
 
@@ -174,16 +172,12 @@ Game.prototype.start = function() {
   this._viewport.init();
   // create player's ship
   this._playerShip = new Ship(ShipTypes.COBRA3, this._playerShipName, this._player);
-  //this._playerShip.coordinates = new Point2d(
-  //  this._viewport.centre.x - (this._playerShip.model.width / 2),
-  //  this._viewport.centre.y - (this._playerShip.model.height / 2)
-  //)
   this._gameObjects.push(this._playerShip);
   // all systems go!
 	setInterval(main, 1000/fps);
 }
 
-var game = new Game();
+const game = new Game();
 
 function main() {
   requestAnimationFrame(game.tick.bind(game));
