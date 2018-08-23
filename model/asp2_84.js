5fc7ec4d0f01df366e870d85e93811ab4334aad9
Justin Pinner <justinpinner@gmail.com>
2018-08-23 14:45:55 +0100

    Asp Mk2 ready for vectors

--- model/asp2_84.js
+++ model/asp2_84.js
@@ -7,6 +7,10 @@ const Asp2_84 = {
     boostSpeed: 300,
     width: 96,  // 70ft
     height: 89, // 65ft
+    scale: {
+        x: 0.31,    // 96/311
+        y: 0.29     // 89/311
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,68 @@ const Asp2_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 96,
+            y: 4,
+            connectsTo: [1,2,6]
+        },
+        {
+            id: 1,
+            x: 1,
+            y: 138,
+            connectsTo: [2,3]
+        },
+        {
+            id: 2,
+            x: 57,
+            y: 105,
+            connectsTo: [4]
+        },
+        {
+            id: 3,
+            x: 54,
+            y: 270,
+            connectsTo: [5]
+        },
+        {
+            id: 4,
+            x: 155,
+            y: 157,
+            connectsTo: [5,8]
+        },
+        {
+            id: 5,
+            x: 155,
+            y: 270,
+            connectsTo: [9]
+        },
+        {
+            id: 6,
+            x: 216,
+            y: 4,
+            connectsTo: [7,8]
+        },
+        {
+            id: 7,
+            x: 310,
+            y: 138,
+            connectsTo: [8,9]
+        },
+        {
+            id: 8,
+            x: 254,
+            y: 105,
+            connectsTo: []
+        },
+        {
+            id: 9,
+            x: 251,
+            y: 270,
+            connectsTo: []
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
