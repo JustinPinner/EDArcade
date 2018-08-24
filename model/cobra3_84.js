f0cae5fec4d566443024c23c665abd8e70fc11d0
Justin Pinner <justinpinner@gmail.com>
2018-08-24 23:00:11 +0100

    Cobra3 vectorised

--- model/cobra3_84.js
+++ model/cobra3_84.js
@@ -5,8 +5,12 @@ const Cobra3_84 = {
     armour: 216,
     maxSpeed: 282,
     boostSpeed: 402,
-    width: 185, // 130ft
-    height: 82, // 65 ft
+    width: 126,
+    height: 92,
+    scale: {
+        x: 0.40,
+        y: 0.44
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -68,6 +72,80 @@ const Cobra3_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 157,
+            y: 0,
+            connectsTo: [1]
+        },
+        {
+            id: 1,
+            x: 157,
+            y: 23,
+            connectsTo: [2, 11]
+        },
+        {
+            id: 2,
+            x: 119,
+            y: 23,
+            connectsTo: [3,5,6]
+        },
+        {
+            id: 3,
+            x: 3,
+            y: 135,
+            connectsTo: [4,5]
+        },
+        {
+            id: 4,
+            x: 3,
+            y: 164,
+            connectsTo: [5]
+        },
+        {
+            id: 5,
+            x: 32,
+            y: 164,
+            connectsTo: [6,7]
+        },
+        {
+            id: 6,
+            x: 157,
+            y: 76,
+            connectsTo: [2,8,11]
+        },
+        {
+            id: 7,
+            x: 157,
+            y: 164,
+            connectsTo: [6,8]
+        },
+        {
+            id: 8,
+            x: 286,
+            y: 164,
+            connectsTo: [9,10,11]
+        },
+        {
+            id: 9,
+            x: 310,
+            y: 164,
+            connectsTo: [10]
+        },
+        {
+            id: 10,
+            x: 310,
+            y: 135,
+            connectsTo: [11]
+        },
+        {
+            id: 11,
+            x: 195,
+            y: 23,
+            connectsTo: []
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         for (var i = 1; i < 3; i++){
