c169e2e232f98288a6df8c42f19adfcf816c8cef
Justin Pinner <justinpinner@gmail.com>
2018-08-25 11:26:17 +0100

    Moray vector-ready

--- model/moray_84.js
+++ model/moray_84.js
@@ -7,6 +7,10 @@ const Moray_84 = {
     boostSpeed: 300,
     width: 82,  // 60ft
     height: 82, // 60ft
+    scale: {
+        x: 0.26,    // 82/316
+        y: 0.3      // 82/270
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,68 @@ const Moray_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 158,
+            y: 2,
+            connectsTo: [1,9,8,4]
+        },
+        {
+            id: 1,
+            x: 3,
+            y: 110,
+            connectsTo: [2,9]
+        },
+        {
+            id: 2,
+            x: 109,
+            y: 267,
+            connectsTo: [3]
+        },
+        {
+            id: 3,
+            x: 206,
+            y: 267,
+            connectsTo: [4,8]
+        },
+        {
+            id: 4,
+            x: 312,
+            y: 110,
+            connectsTo: [8,0]
+        },
+        {
+            id: 5,
+            x: 182,
+            y: 60,
+            connectsTo: [6,7]
+        },
+        {
+            id: 6,
+            x: 130,
+            y: 60,
+            connectsTo: [7]
+        },
+        {
+            id: 7,
+            x: 158,
+            y: 99,
+            connectsTo: []
+        },
+        {
+            id: 8,
+            x: 232,
+            y: 110,
+            connectsTo: [0,9]
+        },
+        {
+            id: 9,
+            x: 86,
+            y: 110,
+            connectsTo: [0,2]
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
