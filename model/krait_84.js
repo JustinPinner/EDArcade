22e34d87ae051212d85be49c8c66a3073e56593a
Justin Pinner <justinpinner@gmail.com>
2018-08-25 10:02:44 +0100

    Krait + vectors = yes

--- model/krait_84.js
+++ model/krait_84.js
@@ -6,7 +6,11 @@ const Krait_84 = {
     maxSpeed: 260,
     boostSpeed: 300,
     width: 123, // 90ft
-    height: 110,  // 80ft
+    height: 110,  // 80ft,
+    scale: {
+        x: 0.39,    // 123/314
+        y: 0.49     // 110/226
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,68 @@ const Krait_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 158,
+            y: 3,
+            connectsTo: [1,4,7]
+        },
+        {
+            id: 1,
+            x: 3,
+            y: 142,
+            connectsTo: [2,9]
+        },
+        {
+            id: 2,
+            x: 158,
+            y: 221,
+            connectsTo: [3,7]
+        },
+        {
+            id: 3,
+            x: 158,
+            y: 112,
+            connectsTo: [5,6]
+        },
+        {
+            id: 4,
+            x: 158,
+            y: 80,
+            connectsTo: [0,5,6]
+        },
+        {
+            id: 5,
+            x: 127,
+            y: 120,
+            connectsTo: []
+        },
+        {
+            id: 6,
+            x: 190,
+            y: 120,
+            connectsTo: []
+        },
+        {
+            id: 7,
+            x: 310,
+            y: 142,
+            connectsTo: [0,8]
+        },
+        {
+            id: 8,
+            x: 310,
+            y: 3,
+            connectsTo: []
+        },
+        {
+            id: 9,
+            x: 3,
+            y: 3,
+            connectsTo: []
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
