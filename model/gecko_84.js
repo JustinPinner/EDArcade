0eb45f83b4eabefcb796d84c43d0bcde08b6bf16
Justin Pinner <justinpinner@gmail.com>
2018-08-25 09:36:36 +0100

    Gecko vectors

--- model/gecko_84.js
+++ model/gecko_84.js
@@ -7,6 +7,10 @@ const Gecko_84 = {
     boostSpeed: 300,
     width: 89, // 65ft
     height: 55,  // 40ft
+    scale: {
+        x: 0.28,    // 89/320
+        y: 0.26     // 55/214
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,44 @@ const Gecko_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 125,
+            y: 4,
+            connectsTo: [1,2,5]
+        },
+        {
+            id: 1,
+            x: 6,
+            y: 175,
+            connectsTo: [2]
+        },
+        {
+            id: 2,
+            x: 93,
+            y: 210,
+            connectsTo: [3]
+        },
+        {
+            id: 3,
+            x: 228,
+            y: 210,
+            connectsTo: [4,5]
+        },
+        {
+            id: 4,
+            x: 314,
+            y: 175,
+            connectsTo: [5]
+        },
+        {
+            id: 5,
+            x: 200,
+            y: 4,
+            connectsTo: []
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
