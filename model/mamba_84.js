74e75841b2a4340b021363c838e236ac941855a7
Justin Pinner <justinpinner@gmail.com>
2018-08-25 11:04:02 +0100

    Mamba vectors

--- model/mamba_84.js
+++ model/mamba_84.js
@@ -7,6 +7,10 @@ const Mamba_84 = {
     boostSpeed: 300,
     width: 89,  // 65ft
     height: 75, // 55ft
+    scale: {
+        x: 0.28,   // 89/318
+        y: 0.25    // 75/297
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,62 @@ const Mamba_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 158,
+            y: 3,
+            connectsTo: [1,2,3,4]
+        },
+        {
+            id: 1,
+            x: 2,
+            y: 291,
+            connectsTo: [2]
+        },
+        {
+            id: 2,
+            x: 76,
+            y: 291,
+            connectsTo: [3]
+        },
+        {
+            id: 3,
+            x: 240,
+            y: 291,
+            connectsTo: [4]
+        },
+        {
+            id: 4,
+            x: 314,
+            y: 291,
+            connectsTo: []
+        },
+        {
+            id: 5,
+            x: 147,
+            y: 140,
+            connectsTo: [6,8]
+        },
+        {
+            id: 6,
+            x: 171,
+            y: 140,
+            connectsTo: []
+        },
+        {
+            id: 7,
+            x: 174,
+            y: 117,
+            connectsTo: [6,8]
+        },
+        {
+            id: 8,
+            x: 143,
+            y: 117,
+            connectsTo: []
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
