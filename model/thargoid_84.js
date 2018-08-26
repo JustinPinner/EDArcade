252047bd4c8431afb9f584e7380a5dc63c5416b4
Justin Pinner <justinpinner@gmail.com>
2018-08-26 13:21:49 +0100

    Thargoid vectorised

--- model/thargoid_84.js
+++ model/thargoid_84.js
@@ -7,6 +7,10 @@ const Thargoid_84 = {
     boostSpeed: 305,
     width: 247,
     height: 247,
+    scale: {
+        x: 0.82,
+        y: 0.82
+    },
     hardpointGeometry: {
         WEAPON: {
             LARGE: { 
@@ -86,6 +90,104 @@ const Thargoid_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 123,
+            y: 80,
+            connectsTo: [1,8]
+        },
+        {
+            id: 1,
+            x: 82,
+            y: 120,
+            connectsTo: [2,9]
+        },
+        {
+            id: 2,
+            x: 82,
+            y: 177,
+            connectsTo: [10,3]
+        },
+        {
+            id: 3,
+            x: 123,
+            y: 219,
+            connectsTo: [11,4]
+        },
+        {
+            id: 4,
+            x: 177,
+            y: 219,
+            connectsTo: [12,5]
+        },
+        {
+            id: 5,
+            x: 218,
+            y: 177,
+            connectsTo: [13,6]
+        },
+        {
+            id: 6,
+            x: 218,
+            y: 120,
+            connectsTo: [14,7]
+        },
+        {
+            id: 7,
+            x: 177,
+            y: 80,
+            connectsTo: [15,0]
+        },
+        {
+            id: 8,
+            x: 91,
+            y: 5,
+            connectsTo: [9]
+        },
+        {
+            id: 9,
+            x: 4,
+            y: 89,
+            connectsTo: [10]
+        },
+        {
+            id: 10,
+            x: 4,
+            y: 208,
+            connectsTo: [11]
+        },
+        {
+            id: 11,
+            x: 91,
+            y: 295,
+            connectsTo: [12]
+        },
+        {
+            id: 12,
+            x: 208,
+            y: 295,
+            connectsTo: [13]
+        },
+        {
+            id: 13,
+            x: 293,
+            y: 208,
+            connectsTo: [14]
+        },
+        {
+            id: 14,
+            x: 293,
+            y: 89,
+            connectsTo: [15]
+        },
+        {
+            id: 15,
+            x: 208,
+            y: 5,
+            connectsTo: [8]
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         for (var i = 1; i < 4; i++){
