17c12469ae7dea9f4729f5cbc4ab0e727c9d3d50
Justin Pinner <justinpinner@gmail.com>
2018-08-26 12:51:03 +0100

    Shuttle. Vectors.

--- model/shuttle_84.js
+++ model/shuttle_84.js
@@ -5,8 +5,12 @@ const Shuttle_84 = {
     armour: 88,
     maxSpeed: 260,
     boostSpeed: 300,
-    width: 27,
-    height: 48,
+    width: 47,
+    height: 68,
+    scale: {
+        x: 0.24,
+        y: 0.26
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,93 @@ const Shuttle_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 91,
+            y: 5,
+            connectsTo: [1,7,6]
+        },
+        {
+            id: 1,
+            x: 25,
+            y: 49,
+            connectsTo: [2]
+        },
+        {
+            id: 2,
+            x: 24,
+            y: 54,
+            connectsTo: [7,3]
+        },
+        {
+            id: 3,
+            x: 4, 
+            y: 325,
+            connectsTo: [7,4]
+        },
+        {
+            id: 4,
+            x: 185,
+            y: 325,
+            connectsTo: [7,5]
+        },
+        {
+            id: 5,
+            x: 160,
+            y: 49,
+            connectsTo: [7,6]
+        },
+        {
+            id: 6,
+            x: 159,
+            y: 49,
+            connectsTo: [0]
+        },
+        {
+            id: 7,
+            x: 91,
+            y: 46,
+            connectsTo:[]
+        },
+        {
+            id: 8,
+            x: 133,
+            y: 40,
+            connectsTo: [9,10]
+        },
+        {
+            id: 9,
+            x: 97,
+            y: 38,
+            connectsTo: [10]
+        },
+        {
+            id: 10,
+            x: 97,
+            y: 21,
+            connectsTo: []
+        },
+        {
+            id: 11,
+            x: 85,
+            y: 21,
+            connectsTo: [12,13]
+        },
+        {
+            id: 12,
+            x: 85,
+            y: 38,
+            connectsTo: [13]
+        },
+        {
+            id: 13,
+            x: 51,
+            y: 40,
+            connectsTo: []
+        }
+
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
