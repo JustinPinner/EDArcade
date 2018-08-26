d8ee6c030f5bb7b1f1065907490e40d04eb5afc2
Justin Pinner <justinpinner@gmail.com>
2018-08-26 23:10:06 +0100

    Worm has vectors

--- model/worm_84.js
+++ model/worm_84.js
@@ -7,6 +7,10 @@ const Worm_84 = {
     boostSpeed: 300,
     width: 32,
     height: 48,
+    scale: {
+        x: 0.14,
+        y: 0.15
+    },
     hardpointGeometry: {
         WEAPON: {
             SMALL: {
@@ -47,6 +51,68 @@ const Worm_84 = {
             }
         }
     },
+    vertices: [
+        {
+            id: 0,
+            x: 83,
+            y: 5,
+            connectsTo: [1,2]
+        },
+        {
+            id: 1,
+            x: 57,
+            y: 72,
+            connectsTo: [2,3]
+        },
+        {
+            id: 2,
+            x: 93,
+            y: 95,
+            connectsTo: [8,4]
+        },
+        {
+            id: 3,
+            x: 3,
+            y: 321,
+            connectsTo: [4]
+        },
+        {
+            id: 4,
+            x: 93,
+            y: 321,
+            connectsTo: [5]
+        },
+        {
+            id: 5,
+            x: 126,
+            y: 321,
+            connectsTo: [8,6]
+        },
+        {
+            id: 6,
+            x: 219,
+            y: 321,
+            connectsTo: [7]
+        },
+        {
+            id: 7,
+            x: 160,
+            y: 72,
+            connectsTo: [8,9]
+        },
+        {
+            id: 8,
+            x: 126,
+            y: 95,
+            connectsTo: [9]
+        },
+        {
+            id: 9,
+            x: 134,
+            y: 5,
+            connectsTo: [0]
+        }
+    ],
     cells: {},
     loadHardpoints: function(self) {
         self._hardpoints.push(new WeaponHardpoint(self, Size.SMALL.value, 1, PulseLaser, HardpointMountTypes.FIXED, 1));
