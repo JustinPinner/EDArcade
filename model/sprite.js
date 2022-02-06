dd22d7596ec2a2cdbf0638f146559b0ead6b8fb0
Justin Pinner <justinpinner@gmail.com>
2022-02-06 09:35:22 +0000

    Whitespace

--- model/sprite.js
+++ model/sprite.js
@@ -1,34 +1,34 @@
 class Sprite {
-    constructor(x, y, width, height, name, cells) {
-        this._coordinates = new Point2d(x, y);
-        this._width = width;
-        this._height = height;
-        this._name = name;
-        this._loadedImage = null;
-        this._cells = cells || {};
-        this.loadImage();
-    }
-    get coordinates() {
-        return this._coordinates;
-    }
-    get width() {
-        return this._width;
-    }
-    get height() {
-        return this._height;
-    }
-    get cells() {
-        return this._cells;
-    }
-    get image() {
-        return this._loadedImage;
-    }
+  constructor(x, y, width, height, name, cells) {
+    this._coordinates = new Point2d(x, y);
+    this._width = width;
+    this._height = height;
+    this._name = name;
+    this._loadedImage = null;
+    this._cells = cells || {};
+    this.loadImage();
+  }
+  get coordinates() {
+    return this._coordinates;
+  }
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+  get cells() {
+    return this._cells;
+  }
+  get image() {
+    return this._loadedImage;
+  }
 
-    set cells(obj) {
-        this._cells = obj;
-    }
+  set cells(obj) {
+    this._cells = obj;
+  }
 }
 
 Sprite.prototype.loadImage = function() {
-    this._loadedImage = imageService.loadImage('../image/' + this._name + '.png');
+  this._loadedImage = imageService.loadImage('../image/' + this._name + '.png');
 }
